d408a76d1efe5c67881786496cb7e8a1c1955ab4
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-05 00:32:51 +0430

    add the navlink and header for the page

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logoImg from '../images/logo.png';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/weatherinfo',
+      text: 'Wheather Info',
+    },
+  ];
+  return (
+    <header>
+      <nav className="navBar">
+        <ul className="pageLinksSection">
+          {links.map((link) => (
+            <li key={link.id}>
+              <NavLink exact="true" to={link.path} className="pageLink">
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
