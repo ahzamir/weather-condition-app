aa47216548b6f60fe712e905413be91a12a655d0
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-05 21:48:39 +0430

    add the action creators for home page categories

--- src/redux/Countries.js
+++ src/redux/Countries.js
@@ -1,3 +1,6 @@
+const GET_COUNTRY = 'my-app/countries/GET_COUNTRY';
+const GET_COUNTRY_STATES = 'my-app/countries/GET_STATES'
+
 const getCountries = async () => {
   const response = await fetch('https://countriesnow.space/api/v0.1/countries/states', {
     method: 'Get',
@@ -7,7 +10,17 @@ const getCountries = async () => {
   });
   const countries = await response.json();
   console.log(countries);
-  return countries;
+  dispatch({
+    type: GET_COUNTRY,
+    payload: countries.data,
+  });
 };
 
-export { getCountries };
+const getCountryStates = (name) => (
+  {
+    type: GET_COUNTRY_STATES,
+    payload: name,
+  }
+);
+
+export { getCountries, getCountryStates };
