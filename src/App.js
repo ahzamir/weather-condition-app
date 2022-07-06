e5c95a0d5bec0c73e647f2e22db4bfc96f1a9282
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-06 23:39:31 +0430

    add a route for states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import States from './components/pages/StatesPage/States';
 import Countries from './components/pages/HomePage/Countries';
 import WeatherInfo from './components/pages/WeatherPage/WeatherInfo';
 
@@ -9,7 +10,8 @@ const App = () => (
     <Navbar />
     <Routes>
       <Route path="/" element={<Countries />} />
-      <Route path="weatherinfo" element={<WeatherInfo />} />
+      <Route path="states" element={<States />} />
+      <Route path="weather" element={<WeatherInfo />} />
     </Routes>
   </>
 );
