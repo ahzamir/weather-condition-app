2bba2cfd5a2a957475e6b8855a7a2ee0b60e9b29
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-06 13:26:03 +0430

    dispatch the getWeather action on state button click

--- src/components/pages/HomePage/State.js
+++ src/components/pages/HomePage/State.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { getWeather } from '../../../redux/WeatherState/weatherState';
 
-const State = ({ state }) => (
-  <button type="button">{state.name}</button>
-);
+const State = ({ state }) => {
+  const dispatch = useDispatch();
+  return (
+    <button
+      type="button"
+      onClick={() => { dispatch(getWeather(state.name)); }}
+    >
+      {state.name}
+    </button>
+  );
+};
 
 export default State;
