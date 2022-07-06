b0af72003926d6cf091e123bef59102db762016e
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-06 13:24:53 +0430

    add the weather store to the state

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import countriesReducers from './CountriesState/countriesState';
+import weatherReducer from './WeatherState/weatherState';
 
 const rootReducer = combineReducers({
   countries: countriesReducers,
+  stateWeather: weatherReducer,
 });
 
 const store = configureStore({ reducer: rootReducer }, applyMiddleware(thunk));
