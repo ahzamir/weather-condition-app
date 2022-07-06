9ad99726b864cd98e1b915d7f9a5a767e8c66e19
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-06 23:42:18 +0430

    convert the data to the key value object

--- src/redux/CountriesState/countriesState.js
+++ src/redux/CountriesState/countriesState.js
@@ -1,5 +1,5 @@
 const GET_COUNTRY = 'my-app/countries/GET_COUNTRY';
-const initialState = [];
+const initialState = {};
 
 const apiGetCountries = async () => {
   const response = await fetch('https://countriesnow.space/api/v0.1/countries/states', {
@@ -14,16 +14,20 @@ const apiGetCountries = async () => {
 
 const getCountries = () => async (dispatch) => {
   const countries = await apiGetCountries();
+  const countriesData = {};
+  countries.data.forEach((country) => {
+    countriesData[country.name] = country;
+  });
   dispatch({
     type: GET_COUNTRY,
-    payload: countries.data,
+    payload: countriesData,
   });
 };
 
 const countriesReducers = (state = initialState, action) => {
   switch (action.type) {
     case GET_COUNTRY:
-      return [...action.payload];
+      return action.payload;
     default: return state;
   }
 };
