63dfb3ba0886faae77bf06c87e588c4e431197f3
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-11-25 18:59:30 +0430

    add a methode to fetch the countries states from api

--- src/redux/CountriesState/provincesState.js
+++ src/redux/CountriesState/provincesState.js
@@ -1,1 +1,16 @@
 const apiUrl = 'https://countriesnow.space/api/v0.1/countries/states';
+
+const apiGetCountriesStates = async (countryName) => {
+    const response = await fetch(`${apiUrl}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            country: countryName,
+        }),
+    });
+    const countriesStates = await response.json();
+    console.log(countriesStates);
+    return countriesStates;
+};
