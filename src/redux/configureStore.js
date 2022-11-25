c366dbb26b4a83d5be81fe8e3c2a7f52e2e69a39
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-11-25 19:42:54 +0430

    added the countriesStates reducer to the root reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import countriesReducers from './CountriesState/countriesState';
+import countryStatesReducers from './CountriesState/countryStatesState';
 import weatherReducer from './WeatherState/weatherState';
 
 const rootReducer = combineReducers({
   countries: countriesReducers,
+  countryStates: countryStatesReducers,
   stateWeather: weatherReducer,
 });
 
