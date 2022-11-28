c69266f8592aa9ee17adfafe46c88ba7c22395db
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-11-28 21:36:46 +0430

    changed the api fetch methode to fetch 3 days forecast data

--- src/redux/WeatherState/weatherState.js
+++ src/redux/WeatherState/weatherState.js
@@ -3,13 +3,14 @@ const GET_WEATHER = 'my-app/states/GET_WEATHER';
 const initialState = {};
 
 const apiGetWeather = async (name) => {
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${name}&aqi=yes`, {
+  const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${name}&days=3&aqi=yes`, {
     method: 'Get',
     headers: {
       'Content-Type': 'application/json',
     },
   });
   const weatherinfo = await response.json();
+  console.log(weatherinfo);
   return weatherinfo;
 };
 
