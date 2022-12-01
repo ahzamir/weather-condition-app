ebdb812dea6a9fa52ffa17da14ac32306d6fb8c3
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-12-01 21:10:45 +0430

    changed the color of footer link to the portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
         <hr />
         <div className="d-flex flex-column justify-content-center align-items-center w-100 p-3">
             <p className="text-white fs-6 fw-bold">© 2022 World Weather</p>
-            <p className="text-white fs-6 fw-bold">Made with ❤️ by <a href="https://my-portfolio-ahzamir.vercel.app/" className="text-decoration-none text-white fw-bold portfolioLink" target={'_blank'}>Ahzamir</a></p>
+            <p className="text-white fs-6 fw-bold">Made with ❤️ by <a href="https://my-portfolio-ahzamir.vercel.app/" className="text-decoration-none fw-bold portfolioLink color-primary" target={'_blank'}>Ahzamir</a></p>
         </div>
     </footer>
 );
