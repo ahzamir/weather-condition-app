d929b9137ba4fff5e4778a243597e3bb6315bf8a
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2023-01-27 23:12:01 +0430

    add some changes to the navbar design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaMicrophone } from 'react-icons/fa';
-import { AiOutlineSetting } from 'react-icons/ai';
 import { IoChevronBackOutline } from 'react-icons/io5';
 
 const Navbar = () => (
-  <header className="primary-bg w-100">
-    <nav>
-      <ul className="d-flex justify-content-between align-items-center w-100 list-style-none p-2">
-        <li className="d-flex align-items-center justify-content-between">
+  <header className="w-100">
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="d-flex justify-content-between align-items-center w-100 list-style-none p-3 p-md-4">
+        <div className="d-flex align-items-center justify-content-between">
           <NavLink exact="true" to="/">
             <IoChevronBackOutline className="text-white fs-5 me-2 fw-bold" />
           </NavLink>
           <NavLink exact="true" to="/" className="text-white fs-5 fw-bold text-decoration-none">
             Home
           </NavLink>
-        </li>
-        <li className="d-flex align-items-center justify-content-between">
-          <FaMicrophone className="text-white fs-5 me-2 fw-bold" />
-          <AiOutlineSetting className="text-white fs-5 fw-bold" />
-        </li>
-      </ul>
+        </div>
+        <div className="d-flex align-items-center justify-content-between">
+          <h1 className="text-white fs-5 fw-bold">
+            World Weather
+          </h1>
+        </div>
+      </div>
     </nav>
   </header>
 );
