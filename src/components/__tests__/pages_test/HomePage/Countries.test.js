4ad5e5759f1fd6f6b5d149286c3bd9461550b1d1
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2023-01-28 18:06:42 +0430

    solved the eslint issues

--- src/components/__tests__/pages_test/HomePage/Countries.test.js
+++ src/components/__tests__/pages_test/HomePage/Countries.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import store from '../../../../redux/configureStore';
 
